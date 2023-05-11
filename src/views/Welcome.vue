c9938b5632ff848c062201a7a2999b5f1a1112c3
Batti-M <bartosz-m@outlook.de>
2023-05-11 14:48:21 +0200

    initial commit

---
+++ src/views/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="welcome container">
+        <p> welcome </p>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.welcome{
+    text-align: center;
+}
+</style>
